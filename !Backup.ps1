232e373c33e768a10694f90c239c70a47e9a547c
John M. Fritsch <johnmfritsch@gmail.com>
2025-06-27 17:10:49 -0400

    test

--- !Backup.ps1
+++ !Backup.ps1
@@ -12,6 +12,7 @@ $date = Get-Date -Format "MM_dd_yyyy"
 
 # Copy project to folder named with date and commit hash
 $source = "C:\Code\Android\BorrowBuddy"
+$destination = "G:\My Drive\Programming\Android\BorrowBuddy - $date - $commitHash"
 $destination = "C:\Code\Android\BorrowBuddy - $date - $commitHash"
 Copy-Item -Path $source -Destination $destination -Recurse -Force
 
